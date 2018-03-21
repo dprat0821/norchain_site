c2ba635e4dcde5f2b71121a26a307403c2990e35
TerrenceRao <rct1985@163.com>
2018-03-21 09:09:21 +0800

    自动上传aws脚本

---
+++ uploadWebGame.sh
@@ -0,0 +1,25 @@
+#Zip Server                                                                                                
+echo "zipWebSite"
+#for the firest time                                                                                       
+#zip -r ./Server_mx_game.zip ./* --exclude=*runtime* -x *log_* .* uploadWebGame.sh                         
+zip -r ./norchain.zip ./website/* --exclude=*runtime* --exclude=*frameworks* -x *log_* .* uploadWebGame.sh
+
+#only publish folder                                                                                       
+#cd ./publish/html5/                                                                                       
+
+
+zip -r ./norchain.zip ./* -X *.DS_Store
+#mv ./Server_mx_game.zip ../../                                                                            
+#cd ../../                                                                                                 
+
+ls -l | grep norchain.zip
+
+scp -i ~/.ssh/zbeans.pem ./norchain.zip ec2-user@amazon_server:~/Documents/
+echo "run deploy_norchain.sh in amazon Server"
+ssh -i ~/.ssh/zbeans.pem ec2-user@amazon_server "cd Documents && ls -la && sh ./deploy_norchain.sh"
+
+
+
+#remove zip file                                                                                           
+echo "remove zip file"
+#rm ./norchain.zip      
